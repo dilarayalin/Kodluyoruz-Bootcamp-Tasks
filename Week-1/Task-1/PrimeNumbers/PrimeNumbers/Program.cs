1789be1c5d6f112ead6b2919fb9ddccdb8e4f575
Dilara Yalin <dilarayalin93@gmail.com>
2022-04-03 13:20:08 +0300

    first commit

---
+++ Week-1/Task-1/PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Program to list prime numbers from 1 to 10000
+             */
+
+            
+            bool isPrime = true;
+
+            for(int i = 2; i<10000; i++)
+            {
+                if(i == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    for(int j = 2; j < i; j++)
+                    {
+                        if(i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if(isPrime)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    isPrime = true;
+                }
+            }
+
+
+        }
+    }
+}
