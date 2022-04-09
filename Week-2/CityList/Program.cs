8f4a7a497f8d55a780945948807e63e530f041df
Dilara Yalin <dilarayalin93@gmail.com>
2022-04-09 09:50:01 +0300

    update

--- Week-2/CityList/Program.cs
+++ Week-2/CityList/Program.cs
@@ -8,11 +8,15 @@ namespace CityList
     {
         static void Main(string[] args)
         {
+            /*
+             * Singular display of repeating records in a list
+             */
+
             List<string> cities = new List<string> {"İstanbul" , "İzmir", "Ankara" , "Niğde", "İstanbul", "Ankara", "İstanbul", "Muğla", "Muğla"};
 
-            List<string> cityList = cities.Distinct().ToList();
+            List<string> cityList = cities.Distinct().ToList(); //We removes duplicate cities from list with Distinct method.
 
-            foreach (var item in cityList)
+            foreach (var item in cityList) //We write non-duplicate elements on the screen.
             {
                 Console.WriteLine(item);
             }
