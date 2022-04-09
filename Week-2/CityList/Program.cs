53045508e4cd1352ed75f1256513ecfa0ffa2dfe
Dilara Yalin <dilarayalin93@gmail.com>
2022-04-09 09:16:17 +0300

    CityList commit

---
+++ Week-2/CityList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CityList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string> {"İstanbul" , "İzmir", "Ankara" , "Niğde", "İstanbul", "Ankara", "İstanbul", "Muğla", "Muğla"};
+
+            List<string> cityList = cities.Distinct().ToList();
+
+            foreach (var item in cityList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
